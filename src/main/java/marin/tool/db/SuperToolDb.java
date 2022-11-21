8fde83841f65924067521c79c714fd4505732bfb
MarinWu <marin_kx@163.com>
2022-11-22 00:57:04 +0800

    新增数据库增删改1

--- src/main/java/marin/tool/db/SuperToolDb.java
+++ src/main/java/marin/tool/db/SuperToolDb.java
@@ -11,10 +11,11 @@ import java.util.Map;
 
 /**
  * @author :Marin Wu
- * @email：marin_kx@163.com
- * @date : 2022/11/21
+ * @email :marin_kx@163.com
+ * @date :2022/11/21
  */
 public class SuperToolDb {
+
 
     /**
      * 和数据库建立连接
@@ -39,6 +40,7 @@ public class SuperToolDb {
      * @return 返回查询结果
      */
     public List<Map<String,Object>> dbSelectAllList(DbConnectionBo dbConnectionBo){
+
         //1、建立连接
         Connection conn = dbConnection(dbConnectionBo);
         //2、QueryRunner对象生成
@@ -59,6 +61,92 @@ public class SuperToolDb {
         return data;
     }
 
+
+    /**
+     * 根据id 和 table 查询一条数据
+     * @return 返回查询结果
+     */
+    public List<Map<String,Object>> dbSelectById(DbConnectionBo dbConnectionBo,Integer id, String table){
+
+        String sql = "SELECT * FROM "+table+" WHERE id = "+ id +" ;";
+
+        //1、建立连接
+        Connection conn = dbConnection(dbConnectionBo);
+        //2、QueryRunner对象生成
+        QueryRunner queryRunner = new QueryRunner();
+        //3、执行sql
+        List<Map<String,Object>> data = null;
+        try {
+            data = queryRunner.query(conn,sql,new MapListHandler());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return data;
+    }
+
+    /**
+     * 根据ids 和单表 查询多条数据
+     * @return 返回查询结果
+     */
+    public List<Map<String,Object>> dbSelectByIds(DbConnectionBo dbConnectionBo,List<Integer> ids, String table){
+
+        String idsData = ids.toString().replaceAll("\\[","").replaceAll("\\]","");
+
+        String sql = "SELECT * FROM "+table+" WHERE id in ("+ idsData +") ;";
+
+        //1、建立连接
+        Connection conn = dbConnection(dbConnectionBo);
+        //2、QueryRunner对象生成
+        QueryRunner queryRunner = new QueryRunner();
+        //3、执行sql
+        List<Map<String,Object>> data = null;
+        try {
+            data = queryRunner.query(conn,sql,new MapListHandler());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return data;
+    }
+
+    /**
+     * 根据ids 和单表 查询多条数据
+     * @return 返回查询结果
+     */
+    public List<Map<String,Object>> dbSelectLike(DbConnectionBo dbConnectionBo,String key, String value, String table){
+
+        String sql = "SELECT * FROM "+table+" WHERE "+key+"LIKE '%"+value+"%';";
+
+        //1、建立连接
+        Connection conn = dbConnection(dbConnectionBo);
+        //2、QueryRunner对象生成
+        QueryRunner queryRunner = new QueryRunner();
+        //3、执行sql
+        List<Map<String,Object>> data = null;
+        try {
+            data = queryRunner.query(conn,sql,new MapListHandler());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return data;
+    }
 
     /**
      * 修改数据库数据操作（插入、修改、删除）
